8475f5ca610153bd1a1cb0c8cf603d760e7d036c
GligorAvram <Gligor_Avram@yahoo.com>
2021-03-27 15:38:37 +0200

    Centered the elements in the gui did some cleanup added info on how oto get a api key

--- src/main/java/ro/gligor/currencyconverter/APIUrl.java
+++ src/main/java/ro/gligor/currencyconverter/APIUrl.java
@@ -6,7 +6,7 @@ public class APIUrl {
     //https://free.currencyconverterapi.com/
 
     public static String getUrlForAPI(String fromCurrency, String toCurrency){
-        String key = "5587a47b2c583c5c19e9";
+        String key = "";
         return("https://free.currconv.com/api/v7/convert?q=" + fromCurrency + "_" + toCurrency + "&compact=ultra&apiKey=" + key);
     }
 }
